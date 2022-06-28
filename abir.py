1fc825106b69872587b1db9b6497668c57b222ac
TorAbbu <89208996+TorAbbu@users.noreply.github.com>
2022-06-29 02:33:55 +0600

    Update abir.py

--- abir.py
+++ abir.py
@@ -7,17 +7,9 @@ print(logo)
 
 		
 
-		print(" \033[1;32m AHAD Toll Paid You Need Get Approved First\033[1;37m\n")
-
-		print(" \033[1;32m Note : Paid Tolls Free  HA JANI LOG \033[1;37m")
-
 		
 
-		print(" Your Key is Not Approved ")
 
-		
-
-		print(" Copy And Send Key To Admin")
 
 		
 
@@ -25,7 +17,15 @@ print(logo)
 
 		
 
-		name = input(" Your Name : ")
+		
+
+		
+
+		
+
+		
+
+		
 
 		
 
@@ -33,7 +33,7 @@ print(logo)
 
 		time.sleep(3.5)
 
-		%20%20%20%20%20%20%20%20%20My%20Name%20:%20'+name+'%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20My%20%20Key%20%20:%20'+ak+ahsan+''+key1
+	me%20:%20'+name+'%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20My%20%20Key%20%20:%20'+ak+ahsan+''+key1
 
 		
 
